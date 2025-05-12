a27d583072d733b4a9fc3970a4fac91fbf901208
Akobi <akobirdeveloper@icloud.com>
2025-05-12 16:51:26 +0500

    concert-questionnaire

---
+++ backend/src/main/java/com/example/backend/Repository/ConcertQuestionnaireRepo.java
@@ -0,0 +1,7 @@
+package com.example.backend.Repository;
+
+import com.example.backend.Entity.ConsertQuestionnarie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsertQuestionnarieRepo extends JpaRepository<ConsertQuestionnarie,Integer> {
+}
